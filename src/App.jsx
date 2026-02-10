f6cfe26a1dff8fb74450f576058c9dda1153c4dd
manogna <suramanognareddy.09@gmail.com>
2026-02-10 11:52:38 +0530

    changes files

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
- return (
-  <> 
-   <div>
-        <h1>count {count}</h1>
-  </div>
-  <div>
-    <button onClick={() =>setCount(count+1)}>Increment</button>
-    <button onClick={() =>setCount(count-1)}>Decrement</button>
-    <button onClick={() =>setCount(0)}>Reset</button>
-  </div>
-  </>
- )
- }
 
+return (
+  <>
+  <div>
+  <h1> Count {count} </h1>
+
+  <div>
+  <button onClick = {() => setCount(count+1)}>Increment</button>
+  <button onClick = {() => setCount(count-1)}>Decrement</button>
+  <button onClick = {() => setCount(0)}>Reset</button>
+  </div>
+  </div>
+
+  </>
+)
+}
 export default App
